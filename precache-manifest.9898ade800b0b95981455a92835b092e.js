e683f8af54ffbe8abfb3c690de3200b1eb55eac3
Gaurav Goyal <gauravgoyal129@gmail.com>
2019-07-17 19:44:21 +0530

    Updates

---
+++ precache-manifest.9898ade800b0b95981455a92835b092e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c19bdc140f5e875edb15e1a853d7b5",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "96b79fab42510851a3a0",
+    "url": "/songs/static/js/2.b8abab9f.chunk.js"
+  },
+  {
+    "revision": "8ceab032133af65de23b",
+    "url": "/songs/static/js/main.46799eac.chunk.js"
+  },
+  {
+    "revision": "650915504906891d68a0",
+    "url": "/songs/static/js/runtime~main.198a175a.js"
+  }
+]);
